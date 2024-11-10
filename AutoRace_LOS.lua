da372e44da9052c5cafadd3a006cd287c9392bef
Demonnic Scripts <alexandregomesap202@gmail.com>
2024-11-10 19:13:50 -0300

    AutoRace_LOS.lua

--- AutoRace_LOS.lua
+++ AutoRace_LOS.lua
@@ -12,6 +12,14 @@ local Section = Tab:AddSection({
 	Name = "Auto Race"
 })
 
+Tab:AddButton({
+    Name = "Karamelo - LOS ⚡",  -- Nome do botão
+    Callback = function() 
+        -- Quando o botão for pressionado, o script será executado
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/DemonnicHub/KarameloScripts/refs/heads/main/AutoRaceKaramelo.lua"))()
+    end    
+})
+
 Tab:AddParagraph("All credits for this script go to @KaramelodeCianuro,\nhe is responsible for all of them, thank you!")
 
 OrionLib:Init()
