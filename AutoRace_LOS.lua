00b1c3df3510e329d303705abfeb4583cb00c56a
Demonnic Scripts <alexandregomesap202@gmail.com>
2024-11-10 18:33:09 -0300

    AutoRace_LOS.lua

--- AutoRace_LOS.lua
+++ AutoRace_LOS.lua
@@ -1,9 +1,9 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/DemonnicHub/KarameloScripts/heads/main/OrionUI.lua')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/DemonnicHub/KarameloScripts/refs/heads/main/OrionUI.lua')))()
 local Window = OrionLib:MakeWindow({Name = "Demonnic Hub X Karamelo | LOS ⚡", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 
 local Tab = Window:MakeTab({
-	Name = "Scripts LOS",
+	Name = "Scripts | LOS",
 	Icon = "rbxassetid://84177443381946",
 	PremiumOnly = false
 })
@@ -13,6 +13,6 @@ local Section = Tab:AddSection({
 })
 
 Tab:AddParagraph("Paragraph","Paragraph Content")
-CoolParagraph:Set("Paragraph New!", "New Paragraph Content!")
+CoolParagraph:Set("All credits for this script go to @KaramelodeCianuro, he is responsible for all of them, thank you!")
 
 OrionLib:Init()
