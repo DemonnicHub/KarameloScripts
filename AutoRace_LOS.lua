8f7b4b8caaac2cc1bf528c5d2b19ec04d110a8e1
Demonnic Scripts <alexandregomesap202@gmail.com>
2024-11-10 17:48:01 -0300

    AutoRace_LOS.lua

--- AutoRace_LOS.lua
+++ AutoRace_LOS.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/DemonnicHub/KarameloScripts/refs/heads/main/RayfieldUI.lua'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Prime",
