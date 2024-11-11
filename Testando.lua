be9a9d1c8c90946fe206283d568bcc566ec6187e
Demonnic Scripts <alexandregomesap202@gmail.com>
2024-11-11 00:11:47 -0300

    Testando.lua

--- Testando.lua
+++ Testando.lua
@@ -476,7 +476,7 @@ local example2 = library:CreateWindow({
 
 local credits= library:CreateWindow({text='Credits'})
 credits:AddLabel("By: @KaramelodeCianuro")
-credits:AddLabel("Edited:@Demonnic_Alexg")
+credits:AddLabel("Edited: @Demonnic_Alexg")
 
 example:AddToggle("Auto Race (Not Working)",function(state)
 	_G.Farm = (state and true or false)
